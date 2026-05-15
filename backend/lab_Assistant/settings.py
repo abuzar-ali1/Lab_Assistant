96f9c0d2b1367e5d29f884c1795fe11f9bcb4da0
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-15 23:57:04 +0500

    Done with migrations

--- backend/lab_Assistant/settings.py
+++ backend/lab_Assistant/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'apps.users'
+    'rest_framework',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
