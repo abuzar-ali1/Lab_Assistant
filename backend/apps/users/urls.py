57faaae43b9d3495f2aaca4ef7d34d5815267c79
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-19 01:01:38 +0500

    jwt done

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from rest_framework_simplejwt.views import ( TokenObtainPairView, TokenRefreshView )
 
 url_patterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import User
 from .serializer import UserSerializer
-
+from rest_framework.generics import CreateAPIView
 
 # Create your views here.
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserRegisterViewSet(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
