eecd1a40487aff31140341148ab4208932bbaa67
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-06-04 23:06:42 +0500

    asdf

--- backend/apps/reports/urls.py
+++ backend/apps/reports/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('upload/', LabReportUploadView.as_view(), name='report-upload'),
     path('', LabReportListView.as_view(), name='report-list'),
     path('<int:report_id>/', LabReportDetailView.as_view(), name='report-detail'),
-    # path('trends/', LabReportTrendView.as_view(), name='report-trend'),   # optional future endpoint
+   
 ]
