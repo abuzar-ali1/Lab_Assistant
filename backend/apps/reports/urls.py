abe313db647aaa0388ff84ebc40564a25b0f150c
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-06-05 23:56:05 +0500

    Nill

--- backend/apps/reports/urls.py
+++ backend/apps/reports/urls.py
@@ -1,1 +1,15 @@
 # reports/urls.py
+from django.urls import path
+from .views import (
+    LabReportUploadView,
+    LabReportDetailView,
+    LabReportListView,
+    
+)
+
+urlpatterns = [
+    path('upload/', LabReportUploadView.as_view(), name='report-upload'),
+    path('', LabReportListView.as_view(), name='report-list'),
+    path('<int:report_id>/', LabReportDetailView.as_view(), name='report-detail'),
+   
+]
