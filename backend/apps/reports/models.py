1669cc47af8747341c73c2d58d9f41295f48c2f1
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-06-10 23:54:41 +0500

    Donw

--- backend/apps/reports/models.py
+++ backend/apps/reports/models.py
@@ -15,7 +15,6 @@ class LabReport(models.Model):
         COMPLETED = 'completed', 'Completed'
         FAILED = 'failed', 'Failed'
 
-    # Primary Key
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
     # User relationship
